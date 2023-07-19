b660ed5a90df8bc5781bb16a1b435d443d191c00
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-19 11:03:52 +0530

    Completed TwoMakesTenTest Cases.java

--- src/twomakesten/TwoMakesTenTest.java
+++ src/twomakesten/TwoMakesTenTest.java
@@ -19,15 +19,37 @@ public class TwoMakesTenTest {
     }
 
     @Test
-    public void giveMinus10_returnFalse() {
+    public void giveMinus10AndMinus10_returnFalse() {
         boolean nineAndTen = twoMakesTen.makesTen(-10, -10);
         assertEquals(false, nineAndTen);
     }
 
     @Test
-    public void giveMinus1_returnFalse() {
+    public void giveMinus1AndMinus1_returnFalse() {
         boolean nineAndTen = twoMakesTen.makesTen(-1, -1);
         assertEquals(false, nineAndTen);
+    }
+    @Test
+    public void giveZeroAndZero_returnFalse() {
+        boolean nineAndTen = twoMakesTen.makesTen(0, 0);
+        assertEquals(false, nineAndTen);
+    }
+    @Test
+    public void giveNineAndTen_returnTrue() {
+        boolean nineAndTen = twoMakesTen.makesTen(9, 10);
+        assertEquals(true, nineAndTen);
+    }
+
+    @Test
+    public void giveOneAndNine_returnTrue() {
+        boolean oneAndNine = twoMakesTen.makesTen(1, 9);
+        assertEquals(true, oneAndNine);
+    }
+
+    @Test
+    public void giveNineAndNine_returnFalse() {
+        boolean nineAndNine = twoMakesTen.makesTen(9, 9);
+        assertEquals(false, nineAndNine);
     }
 }
 
