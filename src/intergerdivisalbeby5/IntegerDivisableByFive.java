8d079e4932cd285722762e93d7fa37e1d515a670
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-19 14:37:31 +0530

    Completed IntegerDivisableByFive.java.Test Cases.java

--- src/intergerdivisalbeby5/IntegerDivisableByFive.java
+++ src/intergerdivisalbeby5/IntegerDivisableByFive.java
@@ -3,6 +3,9 @@ package intergerdivisalbeby5;
 public class IntegerDivisableByFive {
     public boolean find(int number) {
         boolean result = false;
+        if (number % 5 == 0) {
+            return true;
+        }
         return result;
     }
 }
--- src/intergerdivisalbeby5/IntegerDivisableByFiveTest.java
+++ src/intergerdivisalbeby5/IntegerDivisableByFiveTest.java
@@ -21,12 +21,36 @@ public class IntegerDivisableByFiveTest {
     @Test
     public void givenZero_returnTrue() {
         boolean zero = intergerDivisibleByFive.find(0);
-        assertEquals(false, zero);
+        assertEquals(true, zero);
     }
 
     @Test
     public void givenMinus1_returnFalse() {
         boolean zero = intergerDivisibleByFive.find(-1);
         assertEquals(false, zero);
+    }
+
+    @Test
+    public void givenFive_returnTrue() {
+        boolean five = intergerDivisibleByFive.find(5);
+        assertEquals(true, five);
+    }
+
+    @Test
+    public void givenFifteen_returnTrue() {
+        boolean five = intergerDivisibleByFive.find(15);
+        assertEquals(true, five);
+    }
+
+    @Test
+    public void givenThree_returnFalse() {
+        boolean five = intergerDivisibleByFive.find(3);
+        assertEquals(false, five);
+    }
+
+    @Test
+    public void givenNintynine_returnFalse() {
+        boolean five = intergerDivisibleByFive.find(99);
+        assertEquals(false, five);
     }
 }
