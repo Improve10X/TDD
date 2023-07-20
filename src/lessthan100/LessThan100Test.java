d6c7a5ec611af0759127dda8f2e5daf592bd6285
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-20 12:11:40 +0530

    Completed Testing LessThan100Test

--- src/lessthan100/LessThan100Test.java
+++ src/lessthan100/LessThan100Test.java
@@ -6,36 +6,39 @@ import static org.junit.Assert.assertEquals;
 
 public class LessThan100Test {
     LessThan100 lessThan100 = new LessThan100();
- @Test
-    public void empty() {}
+
+    @Test
+    public void empty() {
+    }
 
 
     @Test
     public void givenMinus1AndMinus1_returnTrue() {
-        Boolean number = lessThan100.checkNumbers(-1,-1);
-        assertEquals(true ,number);
+        Boolean number = lessThan100.checkNumbers(-1, -1);
+        assertEquals(true, number);
     }
+
     @Test
     public void givenZeroAndZero_returnTrue() {
-        Boolean number = lessThan100.checkNumbers(0,0);
-        assertEquals(true ,number);
+        Boolean number = lessThan100.checkNumbers(0, 0);
+        assertEquals(true, number);
     }
 
     @Test
     public void givenThreeAndZero_returnTrue() {
-        Boolean number = lessThan100.checkNumbers(3,0);
-        assertEquals(true ,number);
+        Boolean number = lessThan100.checkNumbers(3, 0);
+        assertEquals(true, number);
     }
 
     @Test
     public void checkNumber() {
-        Boolean number = lessThan100.checkNumbers(100,300);
-        assertEquals(false ,number);
+        Boolean number = lessThan100.checkNumbers(100, 300);
+        assertEquals(false, number);
     }
 
     @Test
     public void setLessThan100() {
-        Boolean number = lessThan100.checkNumbers(99,1);
-        assertEquals(false ,number);
+        Boolean number = lessThan100.checkNumbers(99, 1);
+        assertEquals(false, number);
     }
 }
