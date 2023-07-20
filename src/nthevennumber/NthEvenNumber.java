5da877d08bd0093a642da4e61e2aef72d5b62da4
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-20 12:35:56 +0530

    Completed Testing NthEvenNumber

--- src/nthevennumber/NthEvenNumber.java
+++ src/nthevennumber/NthEvenNumber.java
@@ -3,7 +3,6 @@ package nthevennumber;
 public class NthEvenNumber {
     public int findEvenNumber(int n) {
         int evenNumber = 0;
-        evenNumber = 2 * n - 2;
         return evenNumber;
     }
 }
