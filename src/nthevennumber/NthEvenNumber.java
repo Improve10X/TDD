9637187defdb220d3485423872e7eee831b9c437
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-20 12:45:33 +0530

    Completed Testing NthEvenNumber

--- src/nthevennumber/NthEvenNumber.java
+++ src/nthevennumber/NthEvenNumber.java
@@ -3,6 +3,7 @@ package nthevennumber;
 public class NthEvenNumber {
     public int findEvenNumber(int n) {
         int evenNumber = 0;
+        evenNumber = 2 * n - 2;
         return evenNumber;
     }
 }
--- src/nthevennumber/NthEvenNumberTest.java
+++ src/nthevennumber/NthEvenNumberTest.java
@@ -16,11 +16,33 @@ public class NthEvenNumberTest {
     @Test
     public void nothing() {
     }
+    @Test
+    public void givenEvenNumberIsMinusTen_returnEvenNumberIsMinus22() {
+        int evenNumber = nthEvenNumber.findEvenNumber(-10);
+        assertEquals(-22, evenNumber);
+    }
 
     @Test
     public void givenEvenNumberZero_returnEvenNumberIsZero() {
         int evenNumber = nthEvenNumber.findEvenNumber(0);
         assertEquals(0, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsTwo_returnEvenNumberIsEighteen() {
+        int evenNumber = nthEvenNumber.findEvenNumber(2);
+        assertEquals(2, evenNumber);
+    }
+    @Test
+    public void givenEvenNumberIsSix_returnEvenNumberIsEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(6);
+        assertEquals(10, evenNumber);
+    }
+
+    @Test
+    public void givenEvenNumberIsSixty_returnEvenNumberIsOneOneEight() {
+        int evenNumber = nthEvenNumber.findEvenNumber(60);
+        assertEquals(118, evenNumber);
     }
 
 }
