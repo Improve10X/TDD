c59b6e21702c17fd33c3107dfbbe29db8f1349a7
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-24 15:36:04 +0530

    Working on TestCases Fireball.java

--- src/templerungame/Fireball.java
+++ src/templerungame/Fireball.java
@@ -1,8 +1,16 @@
 package templerungame;
 
 public class Fireball {
+    public int speed;
     public Fireball(int speed) {
-        throw new InvalidFireBallSpeedException();
+        if(speed < 0 || speed > 100){
+            throw new InvalidFireBallSpeedException();
+        }
+        this.speed = speed;
+    }
+
+    public void roll() {
+        System.out.println("Fire ball is rolling with " + speed + " Km's Speed" );
     }
 
     public class InvalidFireBallSpeedException extends RuntimeException {
--- src/templerungame/FireballTest.java
+++ src/templerungame/FireballTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -25,4 +26,25 @@ public class FireballTest {
                 "Fireball speed should be between 0 to 100");
     }
 
+    @Test
+    public void given101_throwsInvalidFireBallSpeedException() {
+        assertThrows(Fireball.InvalidFireBallSpeedException.class,
+                () -> new Fireball(101),
+                "Fireball speed should be between 0 to 100");
+    }
+
+
+    @Test
+    public void given10_whenRollMethodCalled_displayRollingMessage() {
+        Fireball fireBall = new Fireball(10);
+        fireBall.roll();
+        assertEquals("Fire ball is rolling with " + 10 + " Km's Speed", outputStream.toString().trim());
+    }
+
+        @Test
+    public void given100_whenRollMethodCalled_displayRollingMessage() {
+        Fireball fireBall = new Fireball(100);
+        fireBall.roll();
+        assertEquals("Fire ball is rolling with " + 100 + " Km's Speed", outputStream.toString().trim());
+    }
 }
