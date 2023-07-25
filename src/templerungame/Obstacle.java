1d15ce6aac8c07967e339629c8639939f3665bed
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-25 14:52:20 +0530

    completed doing Obstacle.java and ObstacleTest

---
+++ src/templerungame/Obstacle.java
@@ -0,0 +1,32 @@
+package templerungame;
+
+public class Obstacle {
+
+    protected String name;
+    protected int damage;
+
+    public Obstacle(String name, int damage) throws InvalidObstacleDamageException {
+        this.name = name;
+        if (damage < 0 || damage > 100) {
+            throw new InvalidObstacleDamageException();
+        }
+        this.damage = damage;
+    }
+
+    public String getName() {
+        return name.trim();
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public void collide(Player player) {
+        System.out.println(name + " has collided with " + player.getName() + " and caused " + damage + " damage.");
+        player.reduceHealth(damage);
+    }
+
+    public class InvalidObstacleDamageException extends Exception {
+
+    }
+}
