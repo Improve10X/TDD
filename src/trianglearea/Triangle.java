5e8d27679698d3e66c58dec36ed7b076b8679f23
SIVAKUMAR <siva.kumar@Improve10x.com>
2023-07-28 15:13:33 +0530

    Completed TempleRun.java

---
+++ src/trianglearea/Triangle.java
@@ -0,0 +1,14 @@
+package trianglearea;
+
+public class Triangle {
+    public int findEdgeOfATriangle(int side1, int side2) {
+        int thirdEdge = (side1 + side2) - 1;
+        if (side1 < 0 && side2 < 0) {
+            return -1;
+        } else if (side1 == 0 && side2 == 0) {
+            return 0;
+        } else {
+            return thirdEdge;
+        }
+    }
+}
